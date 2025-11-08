b1dae914ebdbf274c165bcb0be6131f5b1fbea42
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-11-08 19:45:35 +0100

    first steps

---
+++ frontend/app/components/FullPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="full-page" :style="background">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  background: { type: String, required: true },
+})
+
+const background = {
+    backgroundImage: `url(${props.background})`
+}
+
+</script>
+
+<style>
+.full-page {
+    @apply w-screen;
+    @apply h-screen;
+    overflow-x: hidden;
+
+    background-image: url();
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-home">
+    <FullPage background="/story/01.start.png"/>
+    <FullPage background="/story/02.setup.png"/>
+    <FullPage background="/story/03.honu.png"/>
+    <FullPage background="/story/04.symbol.png"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import FullPage from '../components/FullPage.vue'
+</script>
+
+<style>
+.page-home {
+    @qpply w-screen;
+    overflow-x: hidden;
+}
+</style>
