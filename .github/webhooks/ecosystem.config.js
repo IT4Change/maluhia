b1ac973d43970f00ae2e9f2939811dcecd48110e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-11-09 11:21:16 +0100

    update

--- .github/webhooks/ecosystem.config.js
+++ .github/webhooks/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: "jahrweiser-frontend",
+    name: "frontend",
     script: ".output/server/index.mjs",
     node_args: "-r dotenv/config",
     env_file: ".env" 
