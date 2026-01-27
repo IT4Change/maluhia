04f70625f63af3ca22aa6eec60be21b0ee4209ea
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-01-27 21:43:06 +0100

    no default uuid

--- frontend/server/database/entities/Subscription.ts
+++ frontend/server/database/entities/Subscription.ts
@@ -1,10 +1,9 @@
 import { EntitySchema } from '@mikro-orm/core'
-import { v4 as uuid } from 'uuid'
 
 export class Subscription {
   id!: number
   email!: string
-  confirmationCode: string = uuid()
+  confirmationCode!: string
   confirmed: boolean = false
   createdAt: Date = new Date()
   updatedAt: Date = new Date()
@@ -16,7 +15,7 @@ export const SubscriptionSchema = new EntitySchema<Subscription>({
   properties: {
     id: { type: 'int', primary: true },
     email: { type: 'string', length: 255, unique: true },
-    confirmationCode: { type: 'uuid', onCreate: () => uuid() },
+    confirmationCode: { type: 'uuid' },
     confirmed: { type: 'boolean', default: false },
     createdAt: { type: 'datetime', onCreate: () => new Date() },
     updatedAt: { type: 'datetime', onCreate: () => new Date(), onUpdate: () => new Date() },
